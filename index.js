f242911473a7f7b684bcd7a7b4c2fa116939a122
BIM-Africa <deployment@bim.africa>
2025-11-28 17:44:18 +0500

    index.js

--- index.js
+++ index.js
@@ -57,6 +57,8 @@ app.post("/api/contact", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+
+export default app
